f90423e93f47d3585e15b3b39d880f6d7f68607c
Vitaliy Prilutskiy <Hamachigame@yandex.ru>
2022-11-27 10:33:00 +0700

    Add program

---
+++ Program/Program.cs
@@ -0,0 +1,35 @@
+﻿string [] Create_Array(int n)
+{
+  string[] array = new string[n];
+  for (int i = 0; i < n; i++)
+  {
+    Console.WriteLine("Введи слово");
+    array[i] = Console.ReadLine();
+  }
+  return array;
+}
+
+void Print_Array(string[] array)
+{
+    Console.WriteLine("Массив");
+    foreach (string x in array)
+    {
+        Console.Write(" " + x + " ");
+    }
+    Console.WriteLine(); 
+}
+
+string[] Create_Short_Array(string[] array)
+{
+    string[] short_words = array.Where(element => element.Length <= 3).ToArray();
+    return short_words;
+}
+
+
+Console.WriteLine("Привет, сколько слов вводим?");
+int n = Convert.ToInt32(Console.ReadLine());
+string [] array = Create_Array(n);
+Print_Array(array);
+string [] short_words = Create_Short_Array(array);
+Console.WriteLine("Результат");
+Print_Array(short_words);
